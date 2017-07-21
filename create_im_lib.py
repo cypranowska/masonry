4a7a42ca426dcde3d8b7373f68fff057c79462e9
Sara Calhoun <secalhoun@lbl.gov>
2017-07-20 17:24:18 -0700

    add offset for downloading

--- create_im_lib.py
+++ create_im_lib.py
@@ -34,7 +34,7 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
     return data
 
 #Call this at your own risk
-def download_im(data):
+def download_im(data, offset=0):
     url_list = [val['contentUrl'] for val in data['value']]
     im_format = [val['encodingFormat'] for val in data['value']]
     
@@ -46,21 +46,23 @@ def download_im(data):
 
     for i in range(len(url_list)): 
         if (im_format[i] == 'jpeg' or im_format[i] == 'jpg' or im_format == 'png'):
-            filename = 'img_00' + str(i) + '.' + im_format[i]
+            filename = 'img_00' + str(i+offset) + '.' + im_format[i]
             
-        try:
-            req = urllib.request.Request(im_url[i], headers={'User-Agent': user_agent})
-            img = urllib.request.urlopen(req)
-            output = open(filename, "wb")
-            output.write(img.read())
-            output.close()
-            time.sleep(2)
-        except urllib.error.HTTPError as e:
-            print(e.code)
-            pass
-        except urllib.error.URLError as e:
-            print(e.reason)
-            pass
-        except urllib.request.ssl.CertificateError as e:
-            print (e.reason)
-            pass
+            try:
+                req = urllib.request.Request(im_url[i], headers={'User-Agent': user_agent})
+                img = urllib.request.urlopen(req)
+                output = open(filename, "wb")
+                output.write(img.read())
+                output.close()
+                time.sleep(2)
+            except urllib.error.HTTPError as e:
+                print(e.code)
+                pass
+            except urllib.error.URLError as e:
+                print(e.reason)
+                pass
+            except urllib.request.ssl.CertificateError as e:
+                print (e.reason)
+                pass
+
+    print('Offset to add to count:', data["nextOffsetAddCount"])
