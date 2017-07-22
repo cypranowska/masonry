b247a6ca4ebb7c34debcf06ec7575dbed7720c85
Alvin Kerber <akerber47@gmail.com>
2017-07-22 10:46:46 -0700

    Added command line arguments and progress msgs

--- create_im_lib.py
+++ create_im_lib.py
@@ -1,4 +1,4 @@
-import http.client, urllib.request, urllib.parse, urllib.error, base64, json, time
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json, time, sys, os.path
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
@@ -46,8 +46,8 @@ def download_im(data, offset=0):
 
     for i in range(len(url_list)): 
         if (im_format[i] == 'jpeg' or im_format[i] == 'jpg' or im_format == 'png'):
-            filename = 'img_00' + str(i+offset) + '.' + im_format[i]
-            
+            filename = outputdir + 'img_00' + str(i+offset) + '.' + im_format[i]
+            print("Saving %s..." % filename)
             try:
                 req = urllib.request.Request(im_url[i], headers={'User-Agent': user_agent})
                 img = urllib.request.urlopen(req)
@@ -66,3 +66,25 @@ def download_im(data, offset=0):
                 pass
 
     print('Offset to add to count:', data["nextOffsetAddCount"])
+
+if len(sys.argv) < 5:
+    print("Usage: python create_im_lib.py <api-key> <query> <count> <offset> [<output-dir>] (optional)")
+    sys.exit(2)
+
+apikey = sys.argv[1]
+query = sys.argv[2]
+count = int(sys.argv[3])
+offset = int(sys.argv[4])
+
+if len(sys.argv) > 5:
+    outputdir = sys.argv[5] + "/"
+else:
+    outputdir = "" # current directory
+
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
+
+print("Querying for image URLs...")
+data = im_search(query, count, offset, apikey)
+print("Downloading images...")
+download_im(data, offset)
