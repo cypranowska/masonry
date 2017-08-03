3cb05ca6ce57e63c3b9dd72ac15b5b3ae9d454ec
Caroline Cypranowska <casegura@berkeley.edu>
2017-08-03 01:09:36 -0700

    Update simple_cnn.py

--- simple_cnn/simple_cnn.py
+++ simple_cnn/simple_cnn.py
@@ -79,7 +79,6 @@ def create_model(num_classes):
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
